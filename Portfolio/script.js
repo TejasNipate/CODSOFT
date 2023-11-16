9e383c1e0af20742c9a4a923da9ae3eb76ad3f16
TejasNipate <93865158+TejasNipate@users.noreply.github.com>
2023-11-16 10:32:00 +0530

    Add files via upload

---
+++ Portfolio/script.js
@@ -0,0 +1,5 @@
+document.getElementById("contact-form").addEventListener("submit", function (event) {
+    event.preventDefault();
+    alert("Form submitted! This is a placeholder. You can add actual form submission logic here.");
+});
+
